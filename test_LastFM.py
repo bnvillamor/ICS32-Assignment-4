6239ae158e61a4e20339997c97245e78a483aa78
bnvillamor <benjosephvillamor@gmial.com>
2023-03-10 19:14:57 -0800

    test coverage weather and lastfm

---
+++ test_LastFM.py
@@ -0,0 +1,17 @@
+from LastFM import LastFM
+
+def test_data():
+    lastfm = LastFM()
+    assert lastfm.artist == 'keshi'
+    lastfm.set_apikey('073959fac063bd9930b9ffdb6f4ff9e7')
+    lastfm.load_data()
+    assert lastfm.top_album == 'THE REAPER'
+    assert lastfm.apikey == '073959fac063bd9930b9ffdb6f4ff9e7'
+
+def test_transclude():
+    lastfm = LastFM()
+    lastfm.set_apikey('073959fac063bd9930b9ffdb6f4ff9e7')
+    lastfm.load_data()
+    message = 'Wow I love @lastfm'
+    message2 = lastfm.transclude(message)
+    assert message2 == 'Wow I love THE REAPER'
---
+++ test_OpenWeather.py
@@ -0,0 +1,9 @@
+from OpenWeather import OpenWeather
+
+def test_data():
+    weather = OpenWeather()
+    assert weather.zipcode == '92697'
+    weather.set_apikey('9f6cba8a231f19a48f417e2811537884')
+    weather.load_data()
+    assert weather.city == 'Irvine'
+    assert weather.apikey == '9f6cba8a231f19a48f417e2811537884'
